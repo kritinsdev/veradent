ee3259b760f470dbf0b9a4461459707eef2504e4
Kristaps Ritins <kritinsdev@gmail.com>
2024-10-20 13:19:16 +0300

    add weekly/daily averages for widget

--- app/Filament/Resources/TaskResource/Widgets/EarningsWidget.php
+++ app/Filament/Resources/TaskResource/Widgets/EarningsWidget.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\TaskResource\Widgets;
 use App\Models\Task;
 use Filament\Widgets\Widget;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class EarningsWidget extends Widget
 {
@@ -42,5 +43,41 @@ class EarningsWidget extends Widget
         return Task::whereMonth('created_at', $previousMonth)
             ->whereYear('created_at', $previousMonthYear)
             ->sum('total_price');
+    }
+
+    public function getWeeklyAverageEarnings(): float
+    {
+        $currentYear = Carbon::now()->year;
+
+        $weeklyAverages = Task::select(
+            DB::raw('YEAR(created_at) as year'),
+            DB::raw('WEEK(created_at) as week'),
+            DB::raw('AVG(total_price) as weekly_average')
+        )
+            ->whereYear('created_at', $currentYear)
+            ->groupBy('year', 'week')
+            ->get();
+
+        $totalWeeklyAverage = $weeklyAverages->avg('weekly_average');
+
+        return number_format($totalWeeklyAverage ?? 0, 2);
+    }
+
+    public function getDailyAverageEarnings(): float
+    {
+        $currentYear = Carbon::now()->year;
+
+        $dailyAverages = Task::select(
+            DB::raw('YEAR(created_at) as year'),
+            DB::raw('DAYOFYEAR(created_at) as day_of_year'),
+            DB::raw('AVG(total_price) as daily_average')
+        )
+            ->whereYear('created_at', $currentYear)
+            ->groupBy('year', 'day_of_year')
+            ->get();
+
+        $totalDailyAverage = $dailyAverages->avg('daily_average');
+
+        return number_format($totalDailyAverage ?? 0, 2);
     }
 }
--- resources/views/task-resource/widgets/total-price.blade.php
+++ resources/views/task-resource/widgets/total-price.blade.php
@@ -18,5 +18,19 @@
                 € {{ $this->getTodayEarnings() }}
             </div>
         </x-filament::section>
+
+        <x-filament::section class="flex-1">
+            <span class="text-sm text-gray-500 dark:text-gray-400">Weekly avg</span>
+            <div class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
+                € {{ $this->getWeeklyAverageEarnings() }}
+            </div>
+        </x-filament::section>
+
+        <x-filament::section class="flex-1">
+            <span class="text-sm text-gray-500 dark:text-gray-400">Daily avg</span>
+            <div class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
+                € {{ $this->getDailyAverageEarnings() }}
+            </div>
+        </x-filament::section>
     </div>
 </x-filament-widgets::widget>
