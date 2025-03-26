1d85cd0ce6627254da59e37a7b59cfd4cb22830c
Kristaps Ritins <kritinsdev@gmail.com>
2025-03-26 17:43:06 +0200

    rm force

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
     })->create();
