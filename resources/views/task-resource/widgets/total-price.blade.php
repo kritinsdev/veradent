ac00a92987f339d64fd2020c39cda44a042be122
Kristaps Ritins <kritinsdev@gmail.com>
2025-05-04 13:12:13 +0300

    tweak layout

--- resources/views/task-resource/widgets/total-price.blade.php
+++ resources/views/task-resource/widgets/total-price.blade.php
@@ -1,5 +1,5 @@
 <x-filament-widgets::widget>
-    <div class="flex flex-row gap-4">
+    <div class="flex flex-row flex-wrap gap-4">
         <x-filament::section class="flex-1 flex flex-col">
             <span class="text-sm text-gray-500 dark:text-gray-400">Previous month</span>
             <div class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
